369705442513c9778b42b0acc4ba3d8f06107020
dh0728 <dustkscjswo@naver.com>
2025-05-21 18:00:00 +0900

    pjtList페이지 구현중

---
+++ my-portfolio/src/pages/ProjectListPage.jsx
@@ -0,0 +1,39 @@
+
+const Porject = () => {
+
+    const pjts = [
+
+        { 
+            title:"친환경 크라우드 펀딩 플랫폼 개발", 
+            tools:["SpringBoot", "JAVA"], 
+            myFunc: ["BE 개발", "화면 랜더링 및 결제 API", "채팅"],
+            date: "2025.02.24 ~ 2025.04.11",
+            info: "친환경 상품 펀딩 사업을 전개 및 소비자는 관심있는 친황경 펀딩 상품을 구매할 수 있는 양방향 플랫폼 기획"
+        },
+                { 
+            title:"끌락끌락", 
+            tools:["SpringBoot", "JAVA"], 
+            myFunc: ["BE 개발", "화면 랜더링 및 결제 API", "채팅"],
+            date: "2025.02.24 ~ 2025.04.11",
+            info: ""
+        },
+                { 
+            title:"코드노바", 
+            tools:["React", "JS", "tailwind"], 
+            myFunc: ["FE 개발", "메크로 감지"],
+            date: "2025.02.24 ~ 2025.04.11",
+            info: ""
+        },
+                { 
+            title:"친환경 크라우드 펀딩 플랫폼 개발", 
+            tools:["Django", "Python"], 
+            myFunc: ["BE 개발", "화면 랜더링 및 결제 API", "채팅"],
+            date: "2025.02.24 ~ 2025.04.11",
+            info: ""
+        },
+    ]
+    return (
+
+    );
+
+}
